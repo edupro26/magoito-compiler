20c482e31544d0c12eb5674dba6b1ab998e75d7b
Eduardo Proença <fc57551@alunos.fc.ul.pt>
2026-05-01 23:24:54 +0100

    Remove ANTLR listener generation

--- internal/generate.sh
+++ internal/generate.sh
@@ -5,4 +5,4 @@ set -e
 antlr4='java -jar ./antlr-4.13.2-complete.jar'
 
 $antlr4 -Dlanguage=Go -package lexer -o lexer MagoitoLexer.g4
-$antlr4 -Dlanguage=Go -visitor -package parser -lib lexer -o parser MagoitoParser.g4
+$antlr4 -Dlanguage=Go -no-listener -visitor -package parser -lib lexer -o parser MagoitoParser.g4
